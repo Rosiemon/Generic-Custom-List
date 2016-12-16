85e79a1a1b0007ce2b5cbd4aad9c0bf76f720c0f
Rose Marie <Rose Marie>
2016-12-15 18:04:46 -0600

    fixed bugs and unit tests

--- GenericListUnitTest/UnitTest1.cs
+++ GenericListUnitTest/UnitTest1.cs
@@ -11,15 +11,16 @@ namespace GenericListUnitTest
         public void CheckCountOnAdd1()
         {
             //Arrange
-            CustomList<int> list = new CustomList<int>();
-            list.Count = 0;
-            int expectedResult = 1;
+            CustomList<int> newList = new CustomList<int>();
+            int item = 1;
+            newList.Add(item);
+            int lists = newList.list[0];
 
             //Act
-            list.Add(76);
+            int result = newList.Count;
 
             //Assert
-            Assert.AreEqual(expectedResult, list.Count);
+            Assert.AreEqual(result, 1);
         }
 
         [TestMethod]
@@ -58,6 +59,41 @@ namespace GenericListUnitTest
 
             //Assert
             Assert.AreEqual(expectedResult, list.Count);
+        }
+
+        [TestMethod]
+        public void CheckCountOnAdd4()
+        {
+            //Arrange
+            CustomList<int> newList = new CustomList<int>();
+            newList.Count = 0;
+            int expectedResult = 21;
+
+            //Act
+            newList.Add(1100000);
+            newList.Add(1700);
+            newList.Add(2400);
+            newList.Add(80000);
+            newList.Add(1300);
+            newList.Add(56600);
+            newList.Add(40);
+            newList.Add(5678);
+            newList.Add(9876);
+            newList.Add(3648);
+            newList.Add(9363);
+            newList.Add(2);
+            newList.Add(1100);
+            newList.Add(5200);
+            newList.Add(804);
+            newList.Add(56);
+            newList.Add(5201);
+            newList.Add(5324);
+            newList.Add(644);
+            newList.Add(587);
+            newList.Add(9999);
+
+            //Assert
+            Assert.AreEqual(expectedResult, newList.Count);
         }
 
         [TestMethod]
@@ -103,90 +139,94 @@ namespace GenericListUnitTest
         }
 
         [TestMethod]
+        public void DisplayList3()
+        {
+            //Arrange
+            CustomList<int> newList = new CustomList<int>();
+            newList.Add(1100);
+            newList.Add(5200);
+            newList.Add(804);
+            newList.Add(56);
+            newList.Add(5201);
+            newList.Add(5324);
+            newList.Add(644);
+            newList.Add(587);
+            newList.Add(9999);
+            int list = newList.list[0];
+
+            //Act
+            newList.Display();
+            int actualResult = newList.list[0];
+
+            //Assert
+            Assert.AreEqual(list, actualResult);
+        }
+
+        [TestMethod]
         public void CheckCountOnRemove1()
         {
             //Arrange
-            CustomList<int> list = new CustomList<int>();
-            list.Count = 2;
-            int expectedResult = 1;
+            CustomList<int> newList = new CustomList<int>();
+            newList.Add(1);
+            newList.Add(2);
+            newList.Add(3);
+            newList.Add(4);
+            int expectedResult = 3;
 
             //Act
-            list.Remove(32);
+            newList.Remove(2);
 
             //Assert
-            Assert.AreEqual(expectedResult, list.Count);
+            Assert.AreEqual(expectedResult, newList.Count);
         }
 
         [TestMethod]
         public void CheckCountOnRemove2()
         {
             //Arrange
-            CustomList<int> list = new CustomList<int>();
-            list.Count = 10;
-            int expectedResult = 9;
+            CustomList<int> newList = new CustomList<int>();
+            newList.Add(101);
+            newList.Add(38);
+            newList.Add(72);
+            newList.Add(56);
+            newList.Add(44);
+            newList.Add(8);
+            newList.Add(90);
+            newList.Add(4);
+            int expectedResult = 7;
 
             //Act
-            list.Remove(34);
+            newList.Remove(44);
 
             //Assert
-            Assert.AreEqual(expectedResult, list.Count);
+            Assert.AreEqual(expectedResult, newList.Count);
         }
 
         [TestMethod]
         public void CheckCountOnRemove3()
         {
             //Arrange
-            CustomList<int> list = new CustomList<int>();
-            list.Count = 25;
-            int expectedResult = 24;
-
-            //Act
-            list.Remove(44);
-
-            //Assert
-            Assert.AreEqual(expectedResult, list.Count);
-        }
-
-        [TestMethod]
-        public void CheckIteration1()
-        {
-            //Arrange
             CustomList<int> newList = new CustomList<int>();
-            newList.Add(1);
-            newList.Add(3);
-            newList.Add(5);
-            int item1 = 1;
-            int item2 = 3;
-            int item3 = 5;
-            int list = newList.list[0];
-
-            //Act
-            newList.CheckIteration();
-            int actualResult = newList.list[0];
-
-            //Assert
-            Assert.AreEqual(list, actualResult);
-        }
-
-        [TestMethod]
-        public void CheckIteration2()
-        {
-            //Arrange
-            CustomList<int> newList = new CustomList<int>();
+            newList.Add(2300);
+            newList.Add(1100000);
+            newList.Add(1700);
+            newList.Add(2400);
+            newList.Add(80000);
+            newList.Add(1300);
+            newList.Add(56600);
+            newList.Add(40);
+            newList.Add(5678);
+            newList.Add(9876);
+            newList.Add(3648);
+            newList.Add(9363);
             newList.Add(2);
-            newList.Add(4);
-            newList.Add(6);
-            int item1 = 2;
-            int item2 = 4;
-            int item3 = 6;
-            int list = newList.list[0];
+            int expectedResult = 12;
 
             //Act
-            newList.CheckIteration();
-            int actualResult = newList.list[0];
+            newList.Remove(56600);
 
             //Assert
-            Assert.AreEqual(list, actualResult);
+            Assert.AreEqual(expectedResult, newList.Count);
         }
     }
 }
